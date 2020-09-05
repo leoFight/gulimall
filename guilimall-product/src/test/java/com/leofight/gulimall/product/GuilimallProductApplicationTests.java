879d61ebee7263ccac36bf98e0364e2583150af4
leofight <leofight@test.com>
2020-09-05 14:57:57 +0800

    项目初始结构创建

---
+++ guilimall-product/src/test/java/com/leofight/gulimall/product/GuilimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leofight.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-coupon/src/test/java/com/leofight/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leofight.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
